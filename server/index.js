707b78395df147d5a4475d58d6f970d7f8f7e4e0
conradhappeliv <conrad.h.appel.iv@gmail.com>
2014-08-31 07:45:17 -0500

    server is working with successful long-polling!!
    bring on the frogs

--- server/index.js
+++ server/index.js
@@ -2,46 +2,68 @@ var http = require('http');
 var path = require('path');
 var fs = require('fs');
 
-http.createServer(function(req, res) {
-    if(req.method === 'POST') {
+var Busboy = require('busboy');
+var stat = require('node-static');
+var express = require('express');
+var app = express();
+
+var fileServer = new stat.Server();
+
+var waitingClients = [];
+
+app.post('/', function(req, res) {
+
+    var dt = new Date().getTime();
+    var pathName = __dirname + '/uploads/' + dt + '.gif';
+    // upload gif
+    var busser = new Busboy({headers:req.headers});
+    busser.on('file', function(fieldname, file, filename, encoding, mimetype) {
         if(!fs.existsSync(__dirname + '/uploads')) fs.mkdirSync(__dirname + '/uploads');
-        var pathName = __dirname + '/uploads/' + new Date().getTime() + '.gif';
-        var dest = fs.createWriteStream(pathName);
-        req.pipe(dest);
-        req.on('end', function() {
-            res.writeHead(200);
-            res.end();
-        });
-    } else if(req.method === 'GET') {
-        if(req.headers.accept.match(/text\/html/g)) {
-            fs.readFile('./index.html', 'binary', function(err, file) {
-                if(err) {
-                    console.log(err);
-                    res.writeHead(500);
-                    res.end();
-                } else {
-                    console.log('sent index.html');
-                    res.writeHead(200);
-                    res.write(file);
-                    res.end();
-                }
-            });
-        } else if(req.headers.accept.match(/application\/json/g)) {
-            res.writeHead(200);
-            console.log('json response');
-            var dir = fs.readdirSync('./uploads');
-            var returns = [];
-            for(var i = dir.length - 1; i >= 0 && i > dir.length - 10; i--) {
-                returns.push(dir[i]);
-            }
-            res.write(JSON.stringify(returns));
-            res.end();
-        } else {
-            res.writeHead(404);
-            console.log('invalid req');
-            res.end();
+        file.pipe(fs.createWriteStream(pathName));
+
+    });
+    busser.on('finish', function() {
+        res.writeHead(200);
+        res.end();
+        for(var i = 0; i < waitingClients.length; i++) {
+            var c = waitingClients.pop();
+            c.write(JSON.stringify([dt + '.gif']));
+            c.end();
         }
+    });
+    return req.pipe(busser);
+});
+
+app.get('/gifs', function(req, res) {
+    var files = fs.readdirSync('./uploads');
+    if(req.url.match(files[files.length-1])) {
+        waitingClients.push(res);
+    } else {
+        // send image paths
+        res.writeHead(200);
+        console.log('json response');
+        var dir = fs.readdirSync('./uploads');
+        var returns = [];
+        for(var i = dir.length - 1; i >= 0 && i > dir.length - 10; i--) {
+            returns.push(dir[i]);
+        }
+        res.write(JSON.stringify(returns));
+        res.end();
     }
-}).listen(8888, function() {
-    console.log('server alive on 8888');
+});
+
+app.get('/uploads/*', function(req, res) {
+    req.addListener('end', function() {
+        fileServer.serve(req, res);
+    }).resume();
+});
+
+app.get('/', function(req, res) {
+    req.addListener('end', function() {
+        fileServer.serve(req, res);
+    }).resume();
+});
+
+app.listen(8888, function() {
+    console.log('server alive on port 8888');
 });
